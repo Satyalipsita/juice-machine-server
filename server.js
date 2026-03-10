9a6c25ffe95a1c95880eed6c0048d69e28acf897
Satyalipsita <125855990+Satyalipsita@users.noreply.github.com>
2026-03-10 15:15:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,39 +5,34 @@ app.use(express.json())
 
 let paymentStatus = "WAIT"
 
-// Home page
-app.get("/", (req,res)=>{
+app.get("/",(req,res)=>{
 res.send("Juice Machine Server Running")
 })
 
-// ESP32 checks payment here
 app.get("/check",(req,res)=>{
 res.send(paymentStatus)
 })
 
-// Manual test trigger
 app.get("/pay",(req,res)=>{
 paymentStatus="PAID"
 res.send("Payment received")
 })
 
-// Razorpay webhook
 app.post("/webhook",(req,res)=>{
 
 const event=req.body.event
 
 if(event=="payment.captured"){
 paymentStatus="PAID"
-console.log("Payment captured")
+console.log("Payment captured from Razorpay")
 }
 
 res.status(200).send("OK")
 
 })
 
-const PORT = process.env.PORT || 3000
+const PORT=process.env.PORT||3000
 
 app.listen(PORT,()=>{
-console.log("Server running on port "+PORT)
+console.log("Server running")
 })
-
